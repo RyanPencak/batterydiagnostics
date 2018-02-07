715c93c653d9c4c1286565017964b9bae4301ee8
Ryan Pencak <rvp002@bucknell.edu>
2018-02-07 18:17:47 -0500

    routes

--- server/routes/batteryDataRoutes.js
+++ server/routes/batteryDataRoutes.js
@@ -1,13 +1,24 @@
-module.exports = function(app) {
-  var batteryData = require('../controllers/batteryDataController');
+const router = require('express').Router();
+const batteryData = require('../controllers/batteryDataController');
 
-  // Define Routes
-  app.route('/batteryData')
-    .get(batteryData.list_all_batteries)
-    .post(batteryData.create_battery);
+router.get('/', (req, res, next) => {
+  batteryData.list_all_batteries});
 
-  app.route('/batteryData/:batteryId')
-    .get(batteryData.read_battery)
-    .put(batteryData.update_battery)
-    .delete(batteryData.delete_battery);
-};
+router.get('/:batteryId', (req, res, next) => {
+  batteryData.read_battery});
+
+module.exports = router;
+
+
+//   var batteryData = require('../controllers/batteryDataController');
+//
+//   // Define Routes
+//   app.route('/batteryData')
+//     .get(batteryData.list_all_batteries)
+//     .post(batteryData.create_battery);
+//
+//   app.route('/batteryData/:batteryId')
+//     .get(batteryData.read_battery)
+//     .put(batteryData.update_battery)
+//     .delete(batteryData.delete_battery);
+// };
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends Component {
 
   componentDidMount() {
-    axios.get('/batteryData')
+    axios.get('/api/batteryData')
     .then(({data}) => {console.log(data)})
     .catch(err => {console.log(err)});
   }
