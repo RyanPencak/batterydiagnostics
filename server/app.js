1810dac33f23bd5f3c415a9e96c89c32a863ccda
Ryan Pencak <rvp002@bucknell.edu>
2018-02-08 14:06:44 -0500

    simplify server

--- server/app.js
+++ server/app.js
@@ -30,30 +30,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
-// app.use((req, res, next) => {
-//   const err = new Error('Not Found');
-//
-//   err.status = 404;
-//   next(err);
-// });
-//
-// app.use((err, _req, res, _next) => {
-//   console.log(err);
-//   if (err.status) {
-//     return res
-//     .status(err.status)
-//     .send(err.errors[0].messages[0]);
-//   }
-//
-//   if (err.output && err.output.statusCode) {
-//     return res
-//     .status(err.output.statusCode)
-//     .set('Content-Type', 'text/plain')
-//     .send(err.message);
-//   }
-//
-//   console.error(err.stack);
-//   res.sendStatus(500);
-// });
-
 module.exports = app;
