428a39f1d08f540e104225d200566d352acabe1c
Ryan Pencak <rvp002@bucknell.edu>
2018-03-17 16:59:45 -0500

    format date

--- src/BatteryLog/BatteryLog.js
+++ src/BatteryLog/BatteryLog.js
@@ -22,6 +22,21 @@ export default class BatteryLog extends Component {
     });
   }
 
+  formatDate(date) {
+    var d = date.toString();
+    var day = d.charAt(8) + d.charAt(9);
+    var month = d.charAt(5) + d.charAt(6);
+    var year = d.charAt(0) + d.charAt(1) + d.charAt(2) + d.charAt(3);
+    var hour = d.charAt(11) + d.charAt(12);
+    var hour_temp = parseInt(hour, 10);
+    hour_temp = hour_temp - 5;
+    hour = hour_temp.toString();
+    var minute = d.charAt(14) + d.charAt(15);
+    var second = d.charAt(17) + d.charAt(18);
+
+    return month + '/' + day + '/' + year + ' ' + hour + ':' + minute + ':' + second;
+  }
+
   render() {
 
     return (
@@ -41,7 +56,7 @@ export default class BatteryLog extends Component {
                   <th>Laptop ID</th>
                   <th>% Capacity</th>
                   <th>Battery Quality</th>
-                  <th>Date Tested</th>
+                  <th>Log Date</th>
                 </tr>
               </thead>
               <tbody>
@@ -55,7 +70,7 @@ export default class BatteryLog extends Component {
                       <td>
                         {(battery.mCap[battery.mCap.length - 1] / battery.rCap) > 0.4 ? <Glyphicon glyph="ok" /> : <Glyphicon glyph="remove" />}
                       </td>
-                      <td>{battery.log_date}</td>
+                      <td>{this.formatDate(battery.log_date)}</td>
                     </tr>
                   )
                 })}
