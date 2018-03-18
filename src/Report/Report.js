17b2adeb47a5351718db78edad635f4ced97e594
Ryan Pencak <rvp002@bucknell.edu>
2018-03-17 21:55:18 -0500

    Capacity Progress Circle

--- src/Report/Report.js
+++ src/Report/Report.js
@@ -1,18 +1,39 @@
 import './Report.css';
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import CircularProgressbar from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 // import { LineChart } from 'react-d3-basic';
 
-export default function BatteryLog(props) {
+export default function Report(props) {
 
   const batteryData = props;
 
   return (
-    <div>
+    <div className="Report">
+
       <h2>Battery Report</h2>
-      <h4>Serial Number: {batteryData.serialNum}</h4>
-      <h4>Laptop ID: {batteryData.laptopId}</h4>
-      <h4>Manufacturer Rated Maximum Capacity: {batteryData.rCap} mAh</h4>
-      <h4>Immediate Maximum Capacity: {batteryData.mCap[batteryData.mCap.length-1]} mAh</h4>
+
+      <Grid>
+
+        <Row className="show-grid">
+          <Col xs={12} md={8}>
+            <div className="batterySpecs">
+              <h4>Serial Number: {batteryData.serialNum}</h4>
+              <h4>Laptop ID: {batteryData.laptopId}</h4>
+              <h4>Manufacturer Rated Maximum Capacity: {batteryData.rCap} mAh</h4>
+              <h4>Immediate Maximum Capacity: {batteryData.mCap[batteryData.mCap.length-1]} mAh</h4>
+            </div>
+          </Col>
+          <Col xs={12} md={4}>
+            <div className="batteryCapacity">
+              <CircularProgressbar initialAnimation={true} percentage={(batteryData.mCap[batteryData.mCap.length - 1] / batteryData.rCap) * 100} />
+            </div>
+          </Col>
+        </Row>
+
+      </Grid>
+
     </div>
   );
 }
