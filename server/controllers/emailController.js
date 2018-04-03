609a4471a2dd1c8e411decc7de452968201347f5
Ryan Pencak <rvp002@bucknell.edu>
2018-04-03 10:40:48 -0400

    email templates

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
+const handlebars = require('handlebars');
+const fs = require('fs');
 
 exports.send_email = function(req, res) {
   // url
   const get_url = "https://batterydiagnostics.herokuapp.com/api/battery";
+
+  // read html template email
+  var readHTMLFile = function(path, callback) {
+    fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+        if (err) {
+            throw err;
+            callback(err);
+        }
+        else {
+            callback(null, html);
+        }
+    });
+  };
 
   // nodemailer transporter
   var transporter = nodemailer.createTransport({
@@ -16,24 +31,24 @@ exports.send_email = function(req, res) {
     }
   });
 
-  // function to send battery report
-  function sendReport(email_contents) {
-    // set email data
-    var mailOptions = {
-        from: '"Battery Diagnostics Server" <BatteryDiagnosticServer@gmail.com>', // sender address
-        to: 'BatteryDiagnosticServer@gmail.com', // list of receivers
-        subject: 'Battery Report', // Subject line
-        text: email_contents // plain text body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-    });
-  }
+  // // function to send battery report
+  // function sendReport(email_contents) {
+  //   // set email data
+  //   var mailOptions = {
+  //       from: '"Battery Diagnostics Server" <BatteryDiagnosticServer@gmail.com>', // sender address
+  //       to: 'BatteryDiagnosticServer@gmail.com', // list of receivers
+  //       subject: 'Battery Report', // Subject line
+  //       text: email_contents // plain text body
+  //   };
+  //
+  //   // send mail with defined transport object
+  //   transporter.sendMail(mailOptions, (error, info) => {
+  //       if (error) {
+  //           return console.log(error);
+  //       }
+  //       console.log('Message sent: %s', info.messageId);
+  //   });
+  // }
 
   // call getBatteryData and use data to send email
   // console.log(req.body);
@@ -57,11 +72,61 @@ exports.send_email = function(req, res) {
 
   if(! battery.is_software)
   {
-    sendReport(JSON.stringify(battery));
+    readHTMLFile(__dirname + '/templates/completedTestEmail.html', function(err, html) {
+      var template = handlebars.compile(html);
+      var replacements = {
+           serialNum: battery.serialNum,
+           percentCap: ((battery.mCap[battery.mCap.length-1]/battery.rCap)*100),
+           rCap: battery.rCap,
+           mCap: battery.mCap[battery.mCap.length-1],
+           cycles: battery.cycles
+      };
+      var htmlToSend = template(replacements);
+      var mailOptions = {
+          from: '"Battery Diagnostics Server" <BatteryDiagnosticServer@gmail.com>', // sender address
+          to: 'BatteryDiagnosticServer@gmail.com', // list of receivers
+          subject: 'Battery Test Complete', // Subject line
+          html: htmlToSend
+      };
+      // send mail with defined transport object
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              return console.log(error);
+          }
+          console.log('Message sent: %s', info.messageId);
+      });
+    });
+    // sendReport(JSON.stringify(battery));
   }
   else if((battery.mCap[battery.mCap.length - 1] / battery.rCap) < 0.40)
   {
-    sendReport(JSON.stringify(battery));
+    readHTMLFile(__dirname + '/templates/flaggedBatteryEmail.html', function(err, html) {
+      var template = handlebars.compile(html);
+      var replacements = {
+           serialNum: battery.serialNum,
+           laptopId: battery.laptopId,
+           percentCap: ((battery.mCap[battery.mCap.length-1]/battery.rCap)*100),
+           rCap: battery.rCap,
+           mCap: battery.mCap[battery.mCap.length-1],
+           cycles: battery.cycles
+      };
+      var htmlToSend = template(replacements);
+      var mailOptions = {
+          from: '"Battery Diagnostics Server" <BatteryDiagnosticServer@gmail.com>', // sender address
+          to: 'BatteryDiagnosticServer@gmail.com', // list of receivers
+          subject: 'Battery Flagged', // Subject line
+          html: htmlToSend
+          // html: '<h1> Battery Flagged <h1><h4>Serial Number: {{replacements.serialNum}}</h4><h4>Laptop ID: {{battery.laptopId}}</h4><h4>Manufacturer Rated Maximum Capacity: {{battery.rCap}}</h4><h4>Immediate Maximum Capacity: {{battery.mCap[battery.mCap.length-1]}}</h4><h4>Cycle Count: {{battery.cycles}}</h4>' // html body
+      };
+      // send mail with defined transport object
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              return console.log(error);
+          }
+          console.log('Message sent: %s', info.messageId);
+      });
+    });
+    // sendReport(JSON.stringify(battery));
   }
 
 };
